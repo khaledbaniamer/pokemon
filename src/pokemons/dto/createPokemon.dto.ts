fa4a930f189f4859a0a5321c9bafdad680a2324a
khaled94 <khaledbaniamer23@gamil.com>
2023-12-17 22:16:00 +0200

    create : create and update pokemon dto

---
+++ src/pokemons/dto/createPokemon.dto.ts
@@ -0,0 +1,108 @@
+// pokemon.dto.ts
+import { IsInt, IsString, IsOptional } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsString()
+  name: string;
+
+  @IsString()
+  pokedexNumber: string;
+
+  @IsString()
+  imgName: string;
+
+  @IsInt()
+  generation: number;
+
+  @IsString()
+  @IsOptional()
+  evolutionStage?: string;
+
+  @IsInt()
+  @IsOptional()
+  evolved?: number;
+
+  @IsInt()
+  @IsOptional()
+  familyId?: number;
+
+  @IsInt()
+  @IsOptional()
+  crossGen?: number;
+
+  @IsString()
+  type1: string;
+
+  @IsString()
+  @IsOptional()
+  type2?: string;
+
+  @IsString()
+  weather1: string;
+
+  @IsString()
+  @IsOptional()
+  weather2?: string;
+
+  @IsInt()
+  statTotal: number;
+
+  @IsInt()
+  atk: number;
+
+  @IsInt()
+  def: number;
+
+  @IsInt()
+  sta: number;
+
+  @IsInt()
+  @IsOptional()
+  legendary?: number;
+
+  @IsInt()
+  @IsOptional()
+  aquireable?: number;
+
+  @IsInt()
+  @IsOptional()
+  spawns?: number;
+
+  @IsInt()
+  @IsOptional()
+  regional?: number;
+
+  @IsInt()
+  @IsOptional()
+  raidable?: number;
+
+  @IsInt()
+  @IsOptional()
+  hatchable?: number;
+
+  @IsInt()
+  @IsOptional()
+  shiny?: number;
+
+  @IsInt()
+  @IsOptional()
+  nest?: number;
+
+  @IsInt()
+  @IsOptional()
+  new?: number;
+
+  @IsInt()
+  @IsOptional()
+  notGettable?: number;
+
+  @IsInt()
+  @IsOptional()
+  futureEvolve?: number;
+
+  @IsInt()
+  cp40: number;
+
+  @IsInt()
+  cp39: number;
+}
