70e87e02d92d3a35602e2062b24c0c3f72dca1d7
khaled94 <khaledbaniamer23@gamil.com>
2023-12-17 21:41:03 +0200

    create : prisma service and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PokemonsModule } from './pokemons/pokemons.module';
+import { PrismaModule } from './database/prisma.module';
 
 @Module({
-  imports: [PokemonsModule],
+  imports: [PokemonsModule , PrismaModule],
   controllers: [],
   providers: [],
 })
