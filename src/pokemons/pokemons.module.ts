fbe18b4a76d927cde5bec7dd272d4bf3da821ca8
khaled94 <khaledbaniamer23@gamil.com>
2023-12-21 15:02:26 +0200

    fix : family id in controller test

--- src/pokemons/pokemons.module.ts
+++ src/pokemons/pokemons.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PokemonsController } from './pokemons.controller';
 import { PokemonsService } from './pokemons.service';
-import { PrismaModule } from 'src/database/prisma.module';
+import { PrismaModule } from '../database/prisma.module';
 
 @Module({
   imports: [PrismaModule],
